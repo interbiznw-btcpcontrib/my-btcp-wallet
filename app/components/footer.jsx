b54c25be01b0f3e4d82af117393b7a4119a81d02
J62 <j.owens@interbiznw.com>
2018-03-16 08:51:16 -0700

    changed to wallet.btcprivate.org

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -21,7 +21,7 @@ export default class ZFooter extends React.Component {
         <Container>      
         <Row>
           <Col md="8">
-            <p>MAKE SURE YOU ARE ON <b>WWW.MYBTCPWALLET.COM AND BOOKMARK IT</b></p>
+            <p>MAKE SURE YOU ARE ON <b>wallet.btcprivate.org AND BOOKMARK IT</b></p>
 
             <p>Keys are validated client-side and do not leave your browser or network. You are responsible for keeping your own keys safe! SAVE YOUR PRIVATE KEY BEFORE SENDING OR RECEIVING, LEAVING THE PAGE WILL ERASE EVERYTHING.</p>
 
